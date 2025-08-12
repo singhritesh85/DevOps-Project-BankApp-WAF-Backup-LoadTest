7c3ff7b3f278d888bd21b7a041d625f836857e9f
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-08-12 17:17:40 +0530

    Add files via upload

---
+++ terraform-aks-cluster-waf-with-backup/main/provider.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  subscription_id = "5XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  tenant_id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  features {
+    
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+
+    resource_group {
+      prevent_deletion_if_contains_resources = true    ### All the Resources within the Resource Group must be deleted before deleting the Resource Group.
+    }
+   
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+  }
+}
+
+
+############ If Resource Provider Microsoft.KubernetesConfiguration already registered in subscription then no need to write below section ############
+#resource "azurerm_resource_provider_registration" "kubernetes_configuration" {
+#  name = "Microsoft.KubernetesConfiguration"
+#}
